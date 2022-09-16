c2992aaf001f5298ee39e913270b9c8c847aa7ad
Samuel Barbosa <samuelbs98@gmail.com>
2022-09-16 14:04:20 -0300

    Add Notify plugin into quasar.config

--- quasar.config.js
+++ quasar.config.js
@@ -109,7 +109,7 @@ module.exports = configure((ctx) => ({
     // directives: [],
 
     // Quasar plugins
-    plugins: [],
+    plugins: ['Notify'],
   },
 
   // animations: 'all', // --- includes all animations
